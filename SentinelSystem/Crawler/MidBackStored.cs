ed049081a7afbc959f1b16dc28adeab3821b8b22
Kananek Thongkam <kem@ns.co.th>
2017-05-20 18:01:28 +0700

    add store update data in mbos

---
+++ SentinelSystem/Crawler/MidBackStored.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Travox.Sentinel.Engine;
+using Travox.Systems;
+
+namespace Travox.Sentinel.Crawler
+{
+    public class MidBackStored : Controller
+    {
+        public MidBackStored()
+        {
+            base.OnceTime = true;
+            base.SetIntervel = new TimeSpan(0, 10, 0);
+        }
+
+        public override void Update()
+        {
+            DB db = new DB("travox_system");
+            db.StoredProcedure("sentinel.booking_status_finish");
+            db.StoredProcedure("sentinel.booking_status_payment");
+            db.StoredProcedure("sentinel.session_status");
+            base.Update();
+        }
+    }
+}
